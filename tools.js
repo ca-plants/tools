aff176d9a3608d79b0212e13671e2a5705f4c799
John Kenny <johnkenny54@yahoo.com>
2022-10-26 16:21:07 -0700

    first commit

---
+++ tools.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+console.log( "running" )
+
+import { RPI } from "./lib/rpi.js";
+import commandLineArgs from "command-line-args";
+import { DataLoader } from "@ca-plant-list/ca-plant-list/DataLoader";
+
+const OPTION_DEFS = [
+    { name: "tool", type: String },
+];
+
+const options = commandLineArgs( OPTION_DEFS );
+
+DataLoader.load( "./data" );
+
+switch ( options.tool ) {
+    case "rpi":
+        RPI.analyze();
+        break;
+}
