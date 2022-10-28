e2818616b9993d46c0c0d554c8830d9d981467b5
John Kenny <johnkenny54@yahoo.com>
2022-10-28 12:05:31 -0700

    Tools to check iNat names and IDs.

--- tools.js
+++ tools.js
@@ -3,17 +3,26 @@
 import { RPI } from "./lib/rpi.js";
 import commandLineArgs from "command-line-args";
 import { DataLoader } from "@ca-plant-list/ca-plant-list/DataLoader";
+import { INat } from "./lib/inat.js";
+import { LogMessage } from "./lib/logmessage.js";
 
 const OPTION_DEFS = [
+    { name: "data", type: String, defaultValue: "./data" },
+    { name: "data-inat-taxa", type: String, defaultValue: "inat_taxa.csv" },
     { name: "tool", type: String },
 ];
 
 const options = commandLineArgs( OPTION_DEFS );
 
-DataLoader.load( "./data" );
+DataLoader.load( options.data );
 
 switch ( options.tool ) {
+    case "inat":
+        await INat.analyze( options[ "data-inat-taxa" ] );
+        break;
     case "rpi":
         RPI.analyze();
         break;
 }
+
+LogMessage.write( "./external_data/log.tsv" );
