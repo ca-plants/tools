2542c2c3eaa47244cadb6a792859c38ddf06fbde
John Kenny <johnkenny54@yahoo.com>
2023-04-28 18:40:49 -0700

    Pass custom DataLoader to tools.

--- scripts/ca-plant-tools.js
+++ scripts/ca-plant-tools.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 
-import { DataLoader, Taxa } from "@ca-plant-list/ca-plant-list";
+import { DataLoader } from "@ca-plant-list/ca-plant-list";
 import { Tools } from "../lib/tools.js";
 
-const options = Tools.getCommandLineOptions( DataLoader.getOptionDefs() );
-
-await DataLoader.load( options );
-
-Tools.run( options, Taxa );
+Tools.run( DataLoader );
